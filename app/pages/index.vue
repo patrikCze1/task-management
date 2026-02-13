caf4a262643bdfc342013b89255dffa44402af14
Patrik Stipek <patrik.stipek@inveo.cz>
2026-02-13 18:15:00 +0100

    feat: update dashboard page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,13 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { tasks } = useProjectStore();
+</script>
 
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Dashboard</h1>
+    <h1 class="text-3xl font-bold underline mb-4">Dashboard</h1>
+
+    <div class="grid grid-cols-1 gap-4">
+      <TaskCard v-for="task in tasks" :key="task.uuid" :task="task" />
+    </div>
   </div>
 </template>
